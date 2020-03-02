86dc28ada8facacd23ad01cc67f0798ce88ca09a
shg9411 <shg9411@naver.com>
2020-03-02 18:13:16 +0900

    .

--- tmptmp.py
+++ tmptmp.py
@@ -1,52 +1,30 @@
-q = '''김정혁
-양종환
-오희서
-김소망
-문도윤
-박서연
-황규현
-윤혜서
-정다인
-'''.split()
-w = '''Andy
-Tom
-Barin
-Som
-Andy
-Sandy
-Adam
-Chloe
-Grace
-'''.split()
-e = '''2C4
-1C7
-1C7
-1D10
-1D10
-1E1
-2C4
-1E1
-1C1
-'''.split()
-r = '''andybedro0912@gmail.com
-tom2008-@naver.com
-daren5555@naver.com 
-jesusna@naver.com
-andylove0417@naver.com
-aswell77@naver.com
-adamadam4494@gmail.com
-julhee73@naver.com
-pou903@naver.com
-'''.split()
-tmp = '''
-Gospeaking 온라인 학습 정보
-{} {}                         반명: {}
-아이디: {}    패스워드: 0509
-접속주소: new.gospeaking.com
+import sys
 
-고스피킹은 핸드폰, PC 둘다 학습이 가능하며
-크롬 브라우저에서 원활히 학습 가능합니다.
-(고스피킹 과제를 하기 위해선 헤드셋이 필요합니다.)
-'''
-for i in range(9):
-    print(tmp.format(q[i],w[i],e[i],r[i]))
+N = int(sys.stdin.readline())
+data = [ sys.stdin.readline().strip() for _ in range(N)]
+result = [[] for _ in range(51)]
+for i in data:
+    result[len(i)].append(i)
+for index1 in result:
+    # 길이의 비교
+    if len(index1) == 1:
+        print(*index1)
+    elif len(index1) > 1:
+        compare = [0 for _ in range(len(index1))]
+        # 숫자 더하기 비교
+        for i in range(len(index1)):
+            count = 0
+            for j in range(len(index1[i])):
+                if 48 <= ord(index1[i][j]) <= 57:
+                    count += ord(index1[i][j])%48
+            compare[i] = [i,count]
+        compare.sort(key = lambda x:x[1])
+        for i in range(len(compare)):
+            compare[i] = result[result.index(index1)][compare[i][0]]
+        
+        # 숫자로 비교가 안될 시 정렬
+        if compare == index1:
+            index1.sort()
+            print(*index1,sep='\n')
+        else:
+            print(*compare,sep='\n')
